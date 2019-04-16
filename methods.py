ad4336e27c517c02610ade313c10b3565a6ebcdc
leexiulian <lee_xiu_lian@163.com>
2019-04-16 22:25:58 +0800

    操作字符串的方法

---
+++ methods.py
@@ -0,0 +1,9 @@
+name = "ada lovelace"
+#将每个单词的首字母改为大写
+print(name.title())
+#将字符串中的所有字母改为大写
+print(name.upper())
+#将字符串中的所有字母改为小写
+print(name.lower())
+#等价于print(name.title())
+print("ada lovelace".title())
